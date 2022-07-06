d329a8b52360a06d43f22ba70998c1937ecd87c5
Tim Steeman <tmdev82@gmail.com>
2022-07-06 22:10:41 +0200

    basic web app

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+use yew::prelude::*;
+
+struct App;
+
+impl Component for App {
+    type Message = ();
+    type Properties = ();
+    
+    fn create(ctx: &Context<Self>) -> Self {
+        Self
+    }
+
+    fn view(&self, ctx: &Context<Self>) -> Html {
+        html! {
+            <div>
+                <h1>{"Main page"}</h1>
+            </div>
+        }   
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    wasm_logger::init(wasm_logger::Config::default());
+    yew::start_app::<App>();
 }
